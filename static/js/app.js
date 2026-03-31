d595144e8d71b2b3361024d05d1b3e290285b6d8
Benjamen Alford <benjamen.alford@gmail.com>
2026-03-31 12:19:10 -0500

    Quick Kill #2: Create shell static/js/app.js

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+// benjamenalford.info - Application Shell
+// This file will contain client-side functionality as the site evolves
+
+(function() {
+  'use strict';
+
+  // Placeholder for future functionality
+  console.log('App initialized');
+
+})();
