bf55ff7b127b51e031e3fb73ef4ec631a7052b8e
Benjamen Alford <benjamen.alford@gmail.com>
2026-03-31 14:37:03 -0500

    Add email link to socials section with spam protection

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,16 @@
 // benjamenalford.info - Application Shell
 // This file will contain client-side functionality as the site evolves
 
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
   // Placeholder for future functionality
-  console.log('App initialized');
+  console.log("App initialized");
 
+  // Spam protection for email link
+  const emailLink = document.querySelector('a[title="Email"]');
+  if (emailLink) {
+    const email = "me" + "@" + "benjamenalford.com";
+    emailLink.setAttribute("href", "mailto:" + email);
+  }
 })();
